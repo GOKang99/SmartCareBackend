23ea5aa5c2daf4e9548e03a86eb160a5613fc41c
omer <justletomer@gmail.com>
2025-02-06 12:25:07 +0900

    최초 커밋

---
+++ src/main/java/com/smartcarebackend/SmartCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartcarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCareBackendApplication.class, args);
+	}
+
+}
