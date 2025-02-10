d77e70e0cf196290c25d3fcfd90995973cbcc3e0
dudrhkdspt112 <dudrhkdspt112@gmail.com>
2025-02-10 15:22:30 +0900

    meal 식사량을 String타입변경함

--- src/main/java/com/smartcarebackend/model/Meal.java
+++ src/main/java/com/smartcarebackend/model/Meal.java
@@ -19,44 +19,44 @@ public class Meal {
     @Column(name = "MEA_DT", nullable = false)
     private LocalDateTime meaDt;  // 일자(년월일시분초)
 
-    @Column(name = "FUND_DIS", length = 50)
+    @Column(name = "FUND_DIS", length = 50, nullable = false)
     private String fundDis; //기능장애 (ex:경증,중증)
 
-    @Column(name = "BRE_TP", length = 30)
+    @Column(name = "BRE_TP", length = 30, nullable = false)
     private String breTp; //아침식사유형
 
-    @Column(name = "BRE_QTY")
-    private Long breQty;  // 아침식사량 (예: 1인분, 2인분)
+    @Column(name = "BRE_QTY", length = 20, nullable = false)
+    private String breQty;  // 아침식사량 (예: 1인분, 2인분)
 
     @Column(name = "BRE_TIME", nullable = false)
     private LocalTime breTime;  // 아침식사시간 (예: 08:00)
 
-    @Column(name = "LUN_TP", length = 30)
+    @Column(name = "LUN_TP", length = 30, nullable = false)
     private String lunTp;  // 점심식사유형 (예: "영양식", "일반식")
 
-    @Column(name = "LUN_QTY")
-    private Long lunQty;  // 점심식사량 (예: 1인분, 2인분)
+    @Column(name = "LUN_QTY",length = 20 , nullable = false)
+    private String lunQty;  // 점심식사량 (예: 1인분, 2인분)
 
     @Column(name = "LUN_TIME", nullable = false)
     private LocalTime lunTime;  // 점심식사시간 (예: 12:00)
 
-    @Column(name = "DIN_TP", length = 30)
+    @Column(name = "DIN_TP", length = 30, nullable = false)
     private String dinTp;  // 저녁식사유형 (예: "영양식", "일반식")
 
-    @Column(name = "DIN_QTY")
-    private Long dinQty;  // 저녁식사량 (예: 1인분, 2인분)
+    @Column(name = "DIN_QTY", length = 20 , nullable = false)
+    private String dinQty;  // 저녁식사량 (예: 1인분, 2인분)
 
     @Column(name = "DIN_TIME", nullable = false)
     private LocalTime dinTime;  // 저녁식사시간 (예: 18:00)
 
-    @Column(name = "MOR_SNACK_QTY")
-    private Long morSnackQty;  // 오전간식량 (예: 1인분, 2인분)
+    @Column(name = "MOR_SNACK_QTY", length = 20 , nullable = false)
+    private String morSnackQty;  // 오전간식량 (예: 1인분, 2인분)
 
     @Column(name = "MOR_SNACK_TIME", nullable = false)
     private LocalTime morSnackTime;  // 오전간식시간 (예: 10:00)
 
-    @Column(name = "AFT_SNACK_QTY")
-    private Long aftSnackQty;  // 오후간식량 (예: 1인분, 2인분)
+    @Column(name = "AFT_SNACK_QTY", length = 20 , nullable = false)
+    private String aftSnackQty;  // 오후간식량 (예: 1인분, 2인분)
 
     @Column(name = "AFT_SNACK_TIME", nullable = false)
     private LocalTime aftSnackTime;  // 오후간식시간 (예: 15:00)
