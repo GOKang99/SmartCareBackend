239d6968b610147920703d40432a2b47b3b2d6dd
dudrhkdspt112 <dudrhkdspt112@gmail.com>
2025-02-10 16:14:17 +0900

    활동량 엔티티 추가 및 cist 컬럼네임수정

---
+++ src/main/java/com/smartcarebackend/model/Activity.java
@@ -0,0 +1,36 @@
+package com.smartcarebackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Activity {
+
+    @Id
+    @Column(name = "ACTIVITY_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long activityId; //활동 아이디
+
+    @CreationTimestamp
+    @Column(name = "ACTIVITY_LAST-DT", nullable = false)
+    private LocalDateTime activityLastDt; //최근 측정일
+
+    @Column(name = "ACTIVITY_SKELETAL_MASS", nullable = false)
+    private Double skeletalMass; //골격근량
+
+    @Column(name = "ACTIVITY_WEIGHT", nullable = false)
+    private Double weight; //체중
+
+    @Column(name = "ACTIVITY_HEIGHT", nullable = false)
+    private Double height; //신장
+
+    @Column(name = "ACTIVITY_FAT_MASS", nullable = false)
+    private Double fatMass; //체지방량
+
+    @Column(name = "ACTIVITY_FAT_PERCENT", nullable = false)
+    private Double fatPercent; //체지방률
+}
