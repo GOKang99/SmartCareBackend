423d911a354c719464c5f0bd670529acdbb41a5d
rok <rok@gmail.com>
2025-02-10 14:38:07 +0900

    보호자, 요양사, 권한 엔티티 생성 및 validation depandenciy 추가

--- src/main/java/com/smartcarebackend/model/Guard.java
+++ src/main/java/com/smartcarebackend/model/Guard.java
@@ -1,16 +1,21 @@
 package com.smartcarebackend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
-public class CareGuard {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Guard {
     @Id
-    @Column(name = "GUARD_ID", length = 10, nullable = false)
-    private String guardId;  // 보호자 ID (Primary Key)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "GUARD_I D", length = 10, nullable = false)
+    private Long guardId;  // 보호자 ID (Primary Key)
 
 //    @ManyToOne
 //    @JoinColumn(name = "RES_ID", nullable = false)  // 입소자 ID (Foreign Key)
@@ -20,7 +25,7 @@ public class CareGuard {
     private String guardLogin;  // 보호자 로그인 아이디
 
     @Column(name = "GUARD_PWD", length = 255, nullable = false)
-    private String guardPwd;  // 보호자 비밀번호 (암호화 저장 권장)
+    private String guardPwd;  // 보호자 비밀번호
 
     @Column(name = "GUARD_NAME", length = 30, nullable = false)
     private String guardName;  // 보호자 이름
@@ -31,12 +36,16 @@ public class CareGuard {
     @Column(name = "GUARD_PHONE", length = 15)
     private String guardPhone;  // 보호자 휴대폰 번호
 
-    @Column(name = "GUARD_SSN", length = 13, nullable = false, unique = true)
+    @Column(name = "GUARD_SSN", length = 13, unique = true)
     private String guardSsn;  // 보호자 주민등록번호 (중복 체크)
 
     @Column(name = "GUARD_TOS", nullable = false)
     private boolean guardTos;  // 이용약관 동의 여부
 
-
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE})
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    @JsonBackReference
+    @ToString.Exclude
+    private Role role;
 
 }
