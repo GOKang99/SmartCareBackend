bcc8040e92a8c46f62c9347a6e8fbc17634d1873
JS <smart317@naver.com>
2025-02-13 14:28:59 +0900

    입소자 repository 수정

--- src/main/java/com/smartcarebackend/repositories/ResidentRepository.java
+++ src/main/java/com/smartcarebackend/repositories/ResidentRepository.java
@@ -4,13 +4,12 @@ import com.smartcarebackend.model.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ResidentRepository extends JpaRepository<Resident, Long> {
+    List<Resident> findByResId(Long resId);
 
-    Optional<Resident> findByResName(String resName);
 
-    // resName 유저가 있는지 확인
-    Boolean existsByResName(String resName);
 }
