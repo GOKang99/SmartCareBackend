021887e2e4656fa3f826a0ddbc6c48fb7e0ad9cc
omer <justletomer@gmail.com>
2025-02-25 16:39:14 +0900

    체성분 분석 엔티티 만드는중

---
+++ src/main/java/com/smartcarebackend/model/Composition.java
@@ -0,0 +1,53 @@
+package com.smartcarebackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "composition")
+
+public class Composition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long comId;
+
+    @Column(name = "COM_DT", nullable = false)
+    private LocalDate comDate; // 일자
+
+    @Column(name = "COM_HEIGHT", nullable = false)
+    private Double comHeight; // 신장
+
+    @Column(name = "COM_WEIGHT", nullable = false)
+    private Double comWeight; // 체중
+
+    @Column(name = "COM_SMM", nullable = false)
+    private Double comSmm; // 골격근량
+
+    @Column(name = "COM_BFM", nullable = false)
+    private Double comBfm; // 체지방량
+
+    @Column(name = "COM_PBF", nullable = false)
+    private Double comPbf; // 체지방율
+
+    @Column(name = "COM_BMI", nullable = false)
+    private Double comBmi; // BMI
+
+    @Column(name = "COM_FAT_LVL", nullable = false)
+    private Integer comFatLvl; // 내장지방레벨
+
+    //참조하기
+    @Column(name = "RES_ID", nullable = false)
+    private Long resId; // 입소자 ID
+
+
+    @Column(name = "GIVER_ID", nullable = false)
+    private Long giverId; // 처리자 ID (요양사 ID)
+}
